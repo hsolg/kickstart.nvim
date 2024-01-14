946f5e5306b021114975bc78ce81063fb21d797d
Henrik Solgaard <henriks@tradesolution.no>
2024-01-14 18:59:49 +0100

    Enable Treesitter playground

--- init.lua
+++ init.lua
@@ -266,6 +266,10 @@ require('lazy').setup({
     build = ':TSUpdate',
   },
 
+  {
+    'nvim-treesitter/playground'
+  },
+
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
