963b9e3cca483326ca4b041cd9b3d1eda63204bd
Henrik Solgaard <henriks@tradesolution.no>
2024-01-14 18:56:41 +0100

    Change indentation settings

--- init.lua
+++ init.lua
@@ -38,6 +38,11 @@ I hope you enjoy your Neovim journey,
 P.S. You can delete this when you're done too. It's your config now :)
 --]]
 
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
